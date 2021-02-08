aa5e0dd8b9a4ff20cb876c0d6e5c7c9b0ed67464
Lindsay Esterman <lindsayesterman@gmail.com>
2021-02-08 11:37:51 -0500

    item name slice+ ...

--- src/Item.js
+++ src/Item.js
@@ -5,7 +5,7 @@ export default class Item extends React.Component {
     return (
       <div className="listing">
         <img className="item" src={this.props.url} />
-        <p className="item-name">{this.props.name}</p>
+        <p className="item-name">{this.props.name.length < 25 ? this.props.name : this.props.name.slice(0,23)+"..."}</p>
         <p className="price">${this.props.price}</p>
       </div>
     );
