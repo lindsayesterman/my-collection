7d090539658122bf2509b6a5fba93ea50c857e38
Lindsay Esterman <lindsayesterman@gmail.com>
2021-02-08 12:16:58 -0500

    item name length mobile

--- src/Item.js
+++ src/Item.js
@@ -2,10 +2,17 @@ import React from "react";
 
 export default class Item extends React.Component {
   render() {
+    const x = window.matchMedia("(max-width:449px)");
     return (
       <div className="listing">
         <img className="item" src={this.props.url} />
-        <p className="item-name">{this.props.name.length < 25 ? this.props.name : this.props.name.slice(0,23)+"..."}</p>
+        <p className="item-name">
+          {!x.matches
+            ? this.props.name
+            : (this.props.name.length > 25)
+            ? this.props.name.slice(0, 23) + "..."
+            : this.props.name}
+        </p>
         <p className="price">${this.props.price}</p>
       </div>
     );
